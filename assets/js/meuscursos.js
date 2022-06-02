70395ffce24f9aed7a41764cd3b67aa7ff999d4b
joaoafb <82537300+joaoafb@users.noreply.github.com>
2022-06-02 19:20:40 -0300

    Add files via upload

--- assets/js/meuscursos.js
+++ assets/js/meuscursos.js
@@ -1,19 +1,4 @@
 function meuscursos() {
-    db.collection("cursos" + localStorage.getItem("email")).get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-
-            localStorage.setItem("idcurso", doc.id)
-            db.collection("cursos").doc(localStorage.getItem("idcurso")).get().then((doc) => {
-                if (doc.exists) {
-                    localStorage.setItem("alunos", doc.data().alunos)
-                } else {
-
-                    console.log("No such document!");
-                }
-            }).catch((error) => {
-                console.log("Error getting document:", error);
-            });
-
 
             db.collection("cursos" + localStorage.getItem("email")).get().then((querySnapshot) => {
                 querySnapshot.forEach((doc) => {
@@ -23,62 +8,45 @@ function meuscursos() {
                     localStorage.setItem("titulocurso", doc.data().titulocurso)
                     localStorage.setItem("descricaocurso", doc.data().descricaocurso)
                     localStorage.setItem("valor", doc.data().valor)
-                    var html = '<br><br>	<div  class="card">'
-                    html += '<div class="imgBx">'
-                    html += '   <img src="' + doc.data().data + '" alt="image" class="w-full transition group-hover:rotate-6 group-hover:scale-125">'
-                    html += '	</div>'
-                    html += '<div class="content">'
-                    html += '<div class="details">'
-                    html += '	<h2 style="color:white;">' + doc.data().titulocurso + '<br><span>' + doc.data().categoria + '</span></h2>'
-                    html += '	<div class="data">'
-                    html += '<h3>' + doc.data().quantasaulascurso + '<br><span>Aulas</span></h3>'
-                    html += '	<h3>' + localStorage.getItem("alunos") + '<br><span>Alunos</span></h3>'
-                    html += '<h3>' + doc.data().autor + '<br><span>Autor</span></h3>'
-                    html += '	</div>'
-                    html += '<div class="actionBtn">'
 
-                    html += '</div>'
-                    html += '</div>'
-                    html += '</div>'
-                    html += '</div>'
+                    const div = document.createElement("div");
+                    const a = document.createElement("a")
+                    const img = document.createElement("img")
+                    const div2 = document.createElement("div")
+                    const div3 = document.createElement("div")
+                    const h5 = document.createElement("h5")
+                    const Texth5 = document.createTextNode(doc.data().titulocurso)
+                    const p = document.createElement("p")
+         
+                    const Textp = document.createTextNode(doc.data().descricaocurso)
+ 
+                    div.className = " m-10 p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+                    a.className = 'block overflow-hidden rounded-lg shadow-sm'
+                    img.className =' object-cover w-full h-56'
+                    img.src = doc.data().data
+                    div2.className = 'p-6'
+                    h5.className = 'text-xl text-white font-bold'
+                    p.className = "mt-2 text-sm text-gray-500"
+                   
+                   div.onclick = function assistir() {
+                    localStorage.setItem("assistir", doc.data().idcurso)
+                    location.href = "./assistir.html"
+                }
+                    
+                   p.appendChild(Textp)
+                    h5.appendChild(Texth5)
+                    a.appendChild(h5)
+                    a.appendChild(p)
+                    
+                    a.appendChild(div2)
+                    a.appendChild(img);
+                    div.appendChild(a);
+                   
+                    a.appendChild(div3);
+                    document.getElementById("listcursos").appendChild(div);
+                    
 
-                    document.getElementById("listcursos").innerHTML += html;
-                    //var html = '<div';
-                    //html += '<div  class="-mx-4 flex flex-wrap ">';
-                    //html += '<div style="margin:10px;" class="w-full px-4 md:w-1/2 lg:w-1/3 z-10 py-4 bg-white shadow-md dark:bg-gray-800">'
-                    //html += '<div class="wow fadeInUp group mb-10" data-wow-delay=".1s" style="visibility: visible; animation-delay: 0.1s;">'
-                    //html += ' <div class="mb-8 overflow-hidden rounded">'
-
-                    // html += '   <img src="' + doc.data().data + '" alt="image" class="w-full transition group-hover:rotate-6 group-hover:scale-125">'
-
-                    // html += '  </div>'
-                    // html += '<div>'
-                    // html += ' <span  class="categoria text-white mb-5 inline-block rounded bg-primary py-1 px-4 text-center text-xs font-semibold leading-loose text-white"> ' + doc.data().categoria + '</span><h3>'
-                    // html += '    <h1  class=" text-white mb-4 inline-block text-xl font-semibold text-dark hover:text-primary sm:text-2xl lg:text-xl xl:text-2xl">' + doc.data().titulocurso + '</h1>'
-                    //  html += ' </h3>'
-                    //  html += ' <p class=" text-white text-base text-body-color">' + doc.data().descricaocurso + '</p>'
-                    //  html += '  </div>'
-                    //  html += '<div id="btn"></div>'
-                    // html += ' </div>'
-                    // html += '</div>'
-                    // html += '</div>'
-                    // html += '</div>'
-
-                    //document.getElementById("listcursos").innerHTML += html;
-
-                    var button = document.createElement("button");
-                    var txt = document.createTextNode("Assistir");
-                    button.style.background = '#7e3af2'
-                    button.style.color = "white"
-                    button.style.border = '0'
-                    button.className = 'btn bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'
-                    button.appendChild(txt);
-                    button.onclick = function assistir() {
-                        localStorage.setItem("assistir", doc.data().idcurso)
-                        location.href = "./assistir.html"
-                    }
-
-                    document.querySelector(".actionBtn").appendChild(button);
+                    
 
 
                 });
@@ -86,8 +54,8 @@ function meuscursos() {
 
 
 
-        });
 
-    });
+
+ 
 
 }
